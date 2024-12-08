f698e7bf1a31cee7347d5b7d89f0bee331f80512
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-12-08 16:44:27 +0100

    explain

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="explain",
+    version="0.0.1",
+    packages=find_packages(),
+    url="https://github.com/FedericoBustaffa/explain",
+    author="FedericoBustaffa",
+    install_requires=["numpy", "scikit-learn"],
+)
