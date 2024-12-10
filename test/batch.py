714e93c1d8bc3c91a52c5f083ed6bcb9a9e21632
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-12-10 16:19:33 +0100

    test

---
+++ test/batch.py
@@ -0,0 +1,7 @@
+import single
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+
+if __name__ == "__main__":
+    pass
