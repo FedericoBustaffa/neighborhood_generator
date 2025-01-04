b71e127107431fb3e8fa5c352c547dd651b85238
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-01-04 17:44:39 +0100

    deap

--- deap_complete.py
+++ deap_complete.py
@@ -1,4 +1,5 @@
 import multiprocessing as mp
+import os
 
 import numpy as np
 import pandas as pd
@@ -7,12 +8,18 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 
+import neighborhood_generator as ng
 from complete import get_args, make_predictions
-from neighborhood_generator import genetic
 from ppga import log
 
 
-def deap_init(point, target, model, pool, workers_num: int) -> base.Toolbox:
+def deap_init(
+    X_test: np.ndarray,
+    point: np.ndarray,
+    target: int,
+    model,
+    pool,
+) -> base.Toolbox:
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
     creator.create("Individual", np.ndarray, fitness=getattr(creator, "FitnessMin"))
 
@@ -30,7 +37,7 @@ def deap_init(point, target, model, pool, workers_num: int) -> base.Toolbox:
     )
 
     toolbox.register(
-        "evaluate", genetic.evaluate, point=point, target=target, blackbox=model
+        "evaluate", ng.genetic.evaluate, point=point, target=target, blackbox=model
     )
     toolbox.register("select", tools.selTournament, tournsize=3)
     toolbox.register("mate", tools.cxOnePoint)
@@ -42,7 +49,7 @@ def deap_init(point, target, model, pool, workers_num: int) -> base.Toolbox:
         indpb=0.5,
     )
 
-    toolbox.register("map", pool.map, processes=workers_num)
+    toolbox.register("map", pool.map)
 
     return toolbox
 
@@ -64,12 +71,68 @@ if __name__ == "__main__":
 
     logger.info(f"start explaining of {str(model).removesuffix('()')}")
 
-    df = pd.read_csv("datasets/classification_100_2_2_1_0.csv")
-    X = df[["feature_1", "feature_2"]].to_numpy()
-    y = df["outcome"].to_numpy()
+    # get the datasets
+    filepaths = [fp for fp in os.listdir("datasets") if fp.startswith("classification")]
+    # filepaths = ["classification_100_2_2_1_0.csv"]
+    datasets = [pd.read_csv(f"datasets/{fp}") for fp in filepaths]
+    logger.info(f"preparing to explain {len(datasets)} datasets")
 
-    pool = mp.Pool(args.workers)
-    toolbox = deap_init(pool)
-    pop = getattr(toolbox, "population")(n=100)
-    hof = tools.HallOfFame(100, similar=np.array_equal)
-    population, logbook = algorithms.eaSimple(pop, toolbox, 0.7, 0.3, 5, halloffame=hof)
+    # for every dataset run the blackbox and make explainations
+    results = {
+        "samples": [],
+        "features": [],
+        "classes": [],
+        "clusters": [],
+        "seed": [],
+        "population_size": [],  # single genetic run features
+        "point": [],
+        "class": [],
+        "target": [],
+        "model": [],
+        "min_fitness": [],  # genetic algorithm output
+        "mean_fitness": [],
+        "fitness_std": [],
+        "max_fitness": [],
+        "accuracy": [],
+    }
+
+    population_sizes = [1000, 2000, 4000]
+    for ps in population_sizes:
+        for i, (fp, df) in enumerate(zip(filepaths, datasets)):
+            logger.info(f"dataset {i+1}/{len(datasets)}")
+            logger.info(f"model: {str(model).removesuffix('()')}")
+            logger.info(f"population_size: {ps}")
+
+            test_set, predictions = make_predictions(model, df, 0.2)
+            logger.info(f"predictions to explain: {len(predictions)}")
+
+            # generate neighbors stats
+            # to repeat at least 5 times
+            stats = ng.generate(model, test_set, predictions, ps, args.workers)
+
+            for k in stats:
+                logger.info(f"{k}: {len(stats[k])}")
+
+            # extract dataset specs from filename
+            dataset_specs = fp.removesuffix(".csv").split("_")
+            samples = len(predictions)
+            features = int(dataset_specs[2])
+            classes = int(dataset_specs[3])
+            clusters = int(dataset_specs[4])
+            seed = int(dataset_specs[5])
+
+            results["samples"].extend([samples for _ in range(len(stats["point"]))])
+            results["features"].extend([features for _ in range(len(stats["point"]))])
+            results["classes"].extend([classes for _ in range(len(stats["point"]))])
+            results["clusters"].extend([clusters for _ in range(len(stats["point"]))])
+            results["seed"].extend([seed for _ in range(len(stats["point"]))])
+            results["population_size"].extend([ps for _ in range(len(stats["point"]))])
+
+            for k in stats:
+                results[k].extend(stats[k])
+
+            results_df = pd.DataFrame(results)
+            results_df.to_csv(
+                f"results/{args.output}_{args.model}.csv", header=True, index=False
+            )
+            print(results_df)
